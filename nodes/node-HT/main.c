c99009598aff859f64770fee4e3b2c043b18c068
Michele Forese <michele.forese.personal@gmail.com>
2023-11-29 18:55:10 +0100

    updated node-HT

--- nodes/node-HT/main.c
+++ nodes/node-HT/main.c
@@ -26,95 +26,40 @@
 static saml21_extwake_t extwake = EXTWAKE;
 static hdc3020_t hdc3020;
 static h10_adc_t h10_adc_dev;
+static lora_state_t lora;
 
 static node_HT node_data;
 
-
-void sensors_read(void)
+void read_vcc_vpanel(h10_adc_t *dev)
 {
-    char payload[NODE_HT_SIZE * 1];
-    lora_state_t lora;
+    h10_adc_init(dev, &h10_adc_params[0]);
 
-    memset(&payload, 0, sizeof(payload));
-
-    node_data.header.signature = ACME_SIGNATURE;
-    cpuid_get((void *)(node_data.header.cpuid));
-
-    node_data.header.s_class = NODE_HT_CLASS;
-
-    double temp;
-    double hum;
-
-    if (hdc3020_init(&hdc3020, hdc3020_params) != HDC3020_OK) {
-        puts("[SENSOR hdc3020] FAIL INIT.");
-        return;
-    }
-    if (hdc3020_read(&hdc3020, &temp, &hum) != HDC3020_OK) {
-        puts("[SENSOR hdc3020] FAIL READ.");
-        return;
-    }
-    puts("[SENSOR hdc3020] READ.");
-    hdc3020_deinit(&hdc3020);
-
-    node_data.temperature = (int16_t)(temp * 100.0);
-    node_data.humidity = (uint16_t)(hum * 100.0);
-
-    h10_adc_init(&h10_adc_dev, &h10_adc_params[0]);
-
-    int32_t vcc = h10_adc_read_vcc(&h10_adc_dev);
+    int32_t vcc = h10_adc_read_vcc(dev);
 
     node_data.header.vcc = (uint16_t)(vcc);
     puts("[SENSOR vcc] READ.");
 
     ztimer_sleep(ZTIMER_MSEC, 10);
 
-    int32_t vpanel = h10_adc_read_vcc(&h10_adc_dev);
+    int32_t vpanel = h10_adc_read_vcc(dev);
 
     node_data.header.vpanel = (uint16_t)(vpanel);
     puts("[SENSOR vpanel] READ.");
 
-    h10_adc_deinit(&h10_adc_dev);
+    h10_adc_deinit(dev);
+}
 
-    memcpy(payload, &node_data, NODE_HT_SIZE);
+void send_lora_message(lora_state_t *lora, char *payload, size_t size)
+{
+    memset(lora, 0, sizeof(*lora));
+    lora->bandwidth = DEFAULT_LORA_BANDWIDTH;
+    lora->spreading_factor = DEFAULT_LORA_SPREADING_FACTOR;
+    lora->coderate = DEFAULT_LORA_CODERATE;
+    lora->channel = DEFAULT_LORA_CHANNEL;
+    lora->power = DEFAULT_LORA_POWER;
+    lora->boost = 1;
 
-    char cpuid[CPUID_LEN * 2 + 1];
-
-    fmt_bytes_hex(cpuid, node_data.header.cpuid, CPUID_LEN);
-    cpuid[CPUID_LEN * 2] = 0;
-
-    char payload_hex[NODE_HT_SIZE * 2 + 1];
-
-    #define PAYLOAD_HEX_SIZE sizeof(payload_hex)
-    fmt_bytes_hex(payload_hex, (uint8_t *)&node_data, NODE_HT_SIZE);
-    payload_hex[NODE_HT_SIZE * 2] = 0;
-
-    printf(
-        "SIGNATURE: %04d, NODE_CLASS: %02d, CPUID: %s, VCC: %04d, VPANEL: %04d, TEMP: %05d, HUM: %05d\n",
-        node_data.header.signature,
-        node_data.header.s_class, cpuid,
-        node_data.header.vcc, node_data.header.vpanel,
-        node_data.temperature, node_data.humidity);
-
-    printf(
-        "SIGNATURE: %04x, NODE_CLASS: %02x, CPUID: %s, VCC: %04x, VPANEL: %04x, TEMP: %04x, HUM: %04x\n",
-        node_data.header.signature,
-        node_data.header.s_class, cpuid,
-        node_data.header.vcc, node_data.header.vpanel,
-        node_data.temperature, node_data.humidity);
-
-    puts(payload_hex);
-
-
-    memset(&lora, 0, sizeof(lora));
-    lora.bandwidth = DEFAULT_LORA_BANDWIDTH;
-    lora.spreading_factor = DEFAULT_LORA_SPREADING_FACTOR;
-    lora.coderate = DEFAULT_LORA_CODERATE;
-    lora.channel = DEFAULT_LORA_CHANNEL;
-    lora.power = DEFAULT_LORA_POWER;
-    lora.boost = 1;
-
-
-    if (lora_init(&(lora)) != 0) {
+    if (lora_init(lora) != 0) {
         return;
     }
 
@@ -124,12 +69,81 @@ void sensors_read(void)
 
     iolist_t packet = {
         .iol_base = payload,
-        .iol_len = NODE_HT_SIZE
+        .iol_len = size
     };
 
     lora_write(&packet);
     puts("Lora send command.");
     lora_off();
+}
+
+void node_HT_print(const node_HT *node)
+{
+    char cpu_id[CPUID_LEN * 2 + 1];
+
+    fmt_bytes_hex(cpu_id, node->header.cpuid, CPUID_LEN);
+    cpu_id[CPUID_LEN * 2] = 0;
+
+    printf(
+        "SIGNATURE: %04d, NODE_CLASS: %02d, CPUID: %s, VCC: %04d, VPANEL: %04d, TEMP: %05d, HUM: %05d\n",
+        node->header.signature,
+        node->header.s_class, cpu_id,
+        node->header.vcc, node->header.vpanel,
+        node->temperature, node->humidity);
+
+    printf(
+        "SIGNATURE: %04X, NODE_CLASS: %02X, CPUID: %s, VCC: %04X, VPANEL: %04X, TEMP: %04X, HUM: %04X\n",
+        node->header.signature,
+        node->header.s_class, cpu_id,
+        node->header.vcc, node->header.vpanel,
+        node->temperature, node->humidity);
+
+    char payload_hex[NODE_HT_SIZE * 2 + 1];
+
+    fmt_bytes_hex(payload_hex, (uint8_t *)node, NODE_HT_SIZE);
+    payload_hex[NODE_HT_SIZE * 2] = 0;
+
+    printf("[HEX DATA] %s.\n", payload_hex);
+}
+
+void hdc3020_sensor_read(hdc3020_t *dev, node_HT *node_data)
+{
+    double temp;
+    double hum;
+
+    if (hdc3020_init(dev, hdc3020_params) != HDC3020_OK) {
+        puts("[SENSOR hdc3020] FAIL INIT.");
+        return;
+    }
+    if (hdc3020_read(dev, &temp, &hum) != HDC3020_OK) {
+        puts("[SENSOR hdc3020] FAIL READ.");
+        return;
+    }
+    puts("[SENSOR hdc3020] READ.");
+    hdc3020_deinit(dev);
+
+    node_data->temperature = (int16_t)(temp * 100.0);
+    node_data->humidity = (uint16_t)(hum * 100.0);
+}
+
+void sensors_read(void)
+{
+    char payload[NODE_HT_SIZE * 1];
+
+    memset(&payload, 0, sizeof(payload));
+
+    node_data.header.signature = ACME_SIGNATURE;
+    cpuid_get((void *)(node_data.header.cpuid));
+    node_data.header.s_class = NODE_HT_CLASS;
+
+    hdc3020_sensor_read(&hdc3020, &node_data);
+
+    read_vcc_vpanel(&h10_adc_dev);
+
+    node_HT_print(&node_data);
+
+    memcpy(payload, &node_data, NODE_HT_SIZE);
+    send_lora_message(&lora, payload, NODE_HT_SIZE);
 }
 
 void wakeup_task(void)
